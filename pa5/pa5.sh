05dd4c703ca0bd16dfadb770f68e92598de2d8d4
Hou-I Lin <hlin64@ucsc.edu>
2020-05-11 08:43:50 -0700

    Update pa5.sh

--- pa5/pa5.sh
+++ pa5/pa5.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # cse101-pt.s20
 # usage: pa5.sh
-# (run within your pa1 directory to test your code)
+# (run within your pa5 directory to test your code)
 
 SRCDIR=https://raw.githubusercontent.com/houi-lin/cse101-pt.s20/master/pa5
 
-EXE_ALL=( pa1-func-check.sh pa1-unit-check.sh pa1-make-check.sh )
+EXE_ALL=( pa5-func-check.sh pa5-unit-check.sh pa5-make-check.sh )
 EXE_RANGE=$((${#EXE_ALL[*]} - 1))
 echo $EXE_RANGE
 for i in $(seq 0 $EXE_RANGE); do
