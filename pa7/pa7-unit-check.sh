2bc6e10b57c3f66798f4e801ae3a7f8f6e0bb78d
Hou-I Lin <hlin64@ucsc.edu>
2020-05-31 18:23:25 -0700

    Upload pa7 script

---
+++ pa7/pa7-unit-check.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+SRCDIR=https://raw.githubusercontent.com/houi-lin/cse101-pt.s20/master/pa7
+
+if [ ! -e backup ]; then
+   echo "WARNING: a backup has been created for you in the \"backup\" folder"
+   mkdir backup
+fi
+
+
+cp *.c *.h Makefile backup   # copy all files of importance into backup
+
+curl $SRCDIR/ModelListTest.cpp > ModelListTest.cpp
+
+echo ""
+echo ""
+
+echo "Press Enter To Continue with Dictionary Unit Test"
+read verbose
+
+echo ""
+echo ""
+
+g++ -std=c++11 -Wall -c ModelListTest.cpp List.cpp
+g++ -std=c++11 -Wall -o ModelListTest ModelListTest.o List.o
+
+timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt 
+
+cat ListTest-out.txt 
+
+rm -f *.o ModelListTest* ListTest-out.txt 
+
